2a9ab4cd06b8b190ca98280de532725bace07122
Harold Gutierrez <haroooldg@gmail.com>
2023-12-01 11:57:04 -0800

    login with google phone and email done

---
+++ public/js/firebase.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCCuPWDLAmg7JnqS6eqggdb5Z4m5tLnLqA",
+  authDomain: "cctb-socialnetworks.firebaseapp.com",
+  projectId: "cctb-socialnetworks",
+  storageBucket: "cctb-socialnetworks.appspot.com",
+  messagingSenderId: "970472152421",
+  appId: "1:970472152421:web:708fe6cc53b0c9a35a958c",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,7 @@ firebase.auth().languageCode = "en";
 window.addEventListener("load", function () {
   // first funtion to listen google button
   this.document
-    .getElementById("mdkgoogle")
+    .getElementById("sign-in-google")
     .addEventListener("click", function () {
       var provider = new firebase.auth.GoogleAuthProvider();
       provider.addScope("email");
@@ -12,6 +12,7 @@ window.addEventListener("load", function () {
         .signInWithPopup(provider)
         .then(function (result) {
           console.log("Loggin sucessfully", result.user);
+          window.location.href = "home.html";
         })
         .catch(function (error) {
           console.log("Loggin fail", error);
@@ -21,14 +22,15 @@ window.addEventListener("load", function () {
   this.document
     .getElementById("sing-in-traditional")
     .addEventListener("click", function () {
-      var emailTxt = document.getElementById("email").value;
-      var passwordTxt = document.getElementById("password").value;
+      var emailTxt = document.getElementById("account_email").value;
+      var passwordTxt = document.getElementById("account_passwd").value;
 
       var provider = new firebase.auth()
         .signInWithEmailAndPassword(emailTxt, passwordTxt)
         .then((userCredential) => {
           var user = userCredential.user;
           console.log("Loggin sucessfully", user);
+          window.location.href = "home.html";
         })
         .catch((error) => {
           console.log("Loggin fail", error);
@@ -36,22 +38,15 @@ window.addEventListener("load", function () {
     });
 
   this.document
-    .getElementById("sing-in-phone")
+    .getElementById("sign-in-phone")
     .addEventListener("click", function () {
       window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(
-        "recaptcha-container",
-        {
-          size: "invisible",
-          callback: (response) => {
-            // reCAPTCHA solved, allow signInWithPhoneNumber.
-            onSignInSubmit();
-          },
-        }
+        "recaptcha-container"
       );
+
       const phoneNumber = "+16047200257";
 
       const appVerifier = window.recaptchaVerifier;
-
       firebase
         .auth()
         .signInWithPhoneNumber(phoneNumber, appVerifier)
@@ -59,7 +54,21 @@ window.addEventListener("load", function () {
           // SMS sent. Prompt user to type the code from the message, then sign the
           // user in with confirmationResult.confirm(code).
           window.confirmationResult = confirmationResult;
-          console.log("Loggin sucessfully", confirmationResult);
+          const code = "123456";
+          confirmationResult
+            .confirm(code)
+            .then((result) => {
+              // User signed in successfully.
+              const user = result.user;
+              // ...
+              console.log("Loggin sucessfully", user);
+              window.location.href = "home.html";
+            })
+            .catch((error) => {
+              // User couldn't sign in (bad verification code?)
+              // ...
+              console.log(error);
+            });
         })
         .catch((error) => {
           console.log("Loggin fail", error);
